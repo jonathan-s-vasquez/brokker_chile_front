b5ba33117fc23589ccfbb857e0fb56d9bb6fb87f
Jonathan Soto Vásquez <jonathan.s.vasquez@programmer.net>
2022-08-06 14:33:00 -0400

    configuring eslint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './views/admin/admin.component';
+import { LoginComponent } from './views/login/login.component';
+import { UserComponent } from './views/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },{
+    path: '',
+    component: UserComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewsComponent } from './views/views.component';
+import { UserComponent } from './views/user/user.component';
+import { AdminComponent } from './views/admin/admin.component';
+import { LoginComponent } from './views/login/login.component';
+import { StoreModule } from '@ngrx/store';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewsComponent,
+    UserComponent,
+    AdminComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
