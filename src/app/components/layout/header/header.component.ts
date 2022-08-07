adc3cd3244fd247759325bee8fde2c0506888171
Jonathan Soto Vásquez <jonathan.s.vasquez@programmer.net>
2022-08-07 01:24:15 -0400

    finished footer

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, map, startWith, distinctUntilChanged, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public readonly windowScroll$ = fromEvent(window, 'scroll').pipe(
+    map(_ => window.scrollY),
+    startWith(0),
+    distinctUntilChanged(),
+    shareReplay(1)
+  );
+
   constructor() { }
 
   ngOnInit(): void {
+    this.windowScroll$.subscribe({
+      next: (s) => {
+        console.log(s)
+      }
+    })
   }
 
 }
